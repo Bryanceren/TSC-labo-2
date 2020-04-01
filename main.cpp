1b529d0808d02008f4dd655001264a0435337650
Bryanceren <00139817@uca.edu.sv>
2020-03-31 23:11:48 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <iostream>
+#include "classes.h"
+
+using namespace std;
+
+void readCoordinates(ifstream &file, int n, item* item_list){
+    int e; float r;
+    for(int i=0; i<n; i++){
+      file >> e >> r;
+      item_list[i].setIntFloat(e,r);
+    }
+}
+void printNodes(int n, item* item_list){
+    for(int i=0; i<n; i++){
+        cout<<item_list[i].getX()<<endl;
+    }
+
+}
+
+int main() {
+    char filename[10];
+    string line;
+    mesh m;
+    ifstream file;
+    float k,Q;
+    int nnodes,neltos,ndirich,nneu;
+    
+    do{
+        cout << "Ingrese el nombre del archivo: ";
+        cin >> filename;
+        file.open(filename);
+    }while(!file);
+    
+    file >> k >> Q;
+    file >> nnodes >> neltos >> ndirich >> nneu;
+    
+    file >> line;
+
+    m.setParameters(k, Q);
+    m.setSizes(nnodes, neltos, ndirich, nneu);
+    m.createData();
+
+    readCoordinates(file,nnodes,m.getNodes());
+
+    file.close();
+    
+    //imprimir nodos
+    
+    printNodes(nnodes,m.getNodes());
+    return 0;
+}
